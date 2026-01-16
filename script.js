18d18e9f91fd66cefd7384df43709629d46315fe
Nutriboxfloripa <emanoelamoraesdeoliveira@gmail.com>
2026-01-16 17:10:37 -0300

    Adc batata de cachorro quente

--- script.js
+++ script.js
@@ -78,46 +78,47 @@ document.addEventListener('DOMContentLoaded', () => {
             let modalContent = `<h3>${name}</h3><p>R$${price.toFixed(2)}</p>`;
 
             if (name.includes('Rosti')) {
-                modalContent += `
-                    <fieldset data-role="queijo">
-                        <legend>Escolha o queijo *</legend>
-                        <ul class="option-list">
-                            <li class="option" data-value="Mussarela">Mussarela</li>
-                            <li class="option" data-value="Cheddar">Cheddar</li>
-                            <li class="option" data-value="Requeijão">Requeijão</li>
-                            <li class="option" data-value="Cream chesse">Cream chesse</li>
-                        </ul>
-                    </fieldset>
-                    <fieldset data-role="sabor">
-                        <legend>Escolha o sabor *</legend>
-                        <ul class="option-list">
-                            <li class="option" data-value="Alho-poró com gorgonzola" data-price="43.90">Alho-poró com gorgonzola (R$43,90)</li>
-                            <li class="option" data-value="Lombo desfiado com BBQ" data-price="43.90">Lombo desfiado com BBQ (R$43,90)</li>
-                            <li class="option" data-value="Pizza" data-price="39.90">Pizza (R$39,90)</li>
-                            <li class="option" data-value="Queijo mussarela" data-price="39.90">Queijo mussarela (R$39,90)</li>
-                            <li class="option" data-value="Bacon" data-price="41.90">Bacon (R$41,90)</li>
-                            <li class="option" data-value="Carne de panela" data-price="44.90">Carne de panela (R$44,90)</li>
-                            <li class="option" data-value="Bolonhesa" data-price="41.90">Bolonhesa (R$41,90)</li>
-                            <li class="option" data-value="Calabresa" data-price="42.90">Calabresa (R$42,90)</li>
-                            <li class="option" data-value="Estrogonofe de Frango" data-price="43.90">Estrogonofe de Frango (R$43,90)</li>
-                            <li class="option" data-value="Estrogonofe de Carne" data-price="49.90">Estrogonofe de Carne (R$49,90)</li>
-                            <li class="option" data-value="Estrogonofe de Grão de Bico" data-price="36.90">Estrogonofe de Grão de Bico (R$36,90)</li>
-                            <li class="option" data-value="Frango Desfiado" data-price="41.90">Frango Desfiado (R$41,90)</li>
-                        </ul>
-                    </fieldset>
-                `;
             } else {
                 modalContent += `
-                    <fieldset data-role="queijo">
-                        <legend>Escolha a borda *</legend>
-                        <ul class="option-list">
-                            <li class="option" data-value="Requeijão Cremoso">Requeijão Cremoso</li>
-                            <li class="option" data-value="Cheddar">Cheddar</li>
-                        </ul>
-                    </fieldset>
+                <fieldset data-role="queijo">
+                <legend>Escolha a borda *</legend>
+                <ul class="option-list">
+                <li class="option" data-value="Requeijão Cremoso">Requeijão Cremoso</li>
+                <li class="option" data-value="Cheddar">Cheddar</li>
+                </ul>
+                </fieldset>
                 `;
             }
-
+            modalContent += `
+                <fieldset data-role="queijo">
+                    <legend>Escolha o queijo *</legend>
+                    <ul class="option-list">
+                        <li class="option" data-value="Mussarela">Mussarela</li>
+                        <li class="option" data-value="Cheddar">Cheddar</li>
+                        <li class="option" data-value="Requeijão">Requeijão</li>
+                        <li class="option" data-value="Cream chesse">Cream chesse</li>
+                    </ul>
+                </fieldset>
+                <fieldset data-role="sabor">
+                <legend>Escolha o sabor *</legend>
+                <ul class="option-list">
+                <li class="option" data-value="Cachorro-quente" data-price="36.90">Cachorro-quente (R$36,90)</li>
+                <li class="option" data-value="Carne de panela" data-price="44.90">Carne de panela (R$44,90)</li>
+                <li class="option" data-value="Estrogonofe de Carne" data-price="49.90">Estrogonofe de Carne (R$49,90)</li>
+                <li class="option" data-value="Lombo desfiado com BBQ" data-price="43.90">Lombo desfiado com BBQ (R$43,90)</li>
+                <li class="option" data-value="Estrogonofe de Frango" data-price="43.90">Estrogonofe de Frango (R$43,90)</li>
+                <li class="option" data-value="Bacon" data-price="43.90">Bacon (R$43,90)</li>
+                <li class="option" data-value="Calabresa" data-price="42.90">Calabresa (R$42,90)</li>
+                <li class="option" data-value="Frango Desfiado" data-price="41.90">Frango Desfiado (R$41,90)</li>
+                <li class="option" data-value="Bolonhesa" data-price="41.90">Bolonhesa (R$41,90)</li>
+                <li class="option" data-value="Alho-poró com gorgonzola" data-price="43.90">Alho-poró com gorgonzola (R$43,90)</li>
+                <li class="option" data-value="Pizza" data-price="39.90">Pizza (R$39,90)</li>
+                <li class="option" data-value="Estrogonofe de Grão de Bico" data-price="38.90">Estrogonofe de Grão de Bico (R$38,90)</li>
+                <li class="option" data-value="Queijo mussarela" data-price="39.90">Queijo mussarela (R$39,90)</li>
+                    </ul>
+                </fieldset>
+            `;
+            
             modalContent += `
                 <fieldset>
                     <legend>Adicionais (até 2 opções)</legend>
@@ -253,18 +254,19 @@ document.addEventListener('DOMContentLoaded', () => {
             <fieldset>
                 <legend>Escolha o Sabor *</legend>
                 <ul class="option-list">
-                    <li class="option" data-value="Frango desfiado" data-price="15.90">Frango desfiado R$15,90</li>
-                    <li class="option" data-value="Lombo desfiado com BBQ" data-price="17.90">Lombo desfiado com BBQ R$17,90</li>
-                    <li class="option" data-value="Alho-poró com gorgonzola" data-price="17.90">Alho-poró com gorgonzola R$17,90</li>
-                    <li class="option" data-value="Bacon" data-price="15.90">Bacon R$15,90</li>
-                    <li class="option" data-value="Carne de penela" data-price="18.90">Carne de panela R$18,90</li>
-                    <li class="option" data-value="Calabresa" data-price="16.90">Calabresa R$16,90</li>
-                    <li class="option" data-value="Pizza" data-price="12.90">Pizza R$12,90</li>
-                    <li class="option" data-value="Estrogonofe de frango" data-price="17.90">Estrogonofe de frango R$17,90</li>
-                    <li class="option" data-value="Estrogonofe de carne" data-price="23.90">Estrogonofe de carne R$23,90</li>
-                    <li class="option" data-value="Bolonhesa" data-price="15.90">Bolonhesa R$15,90</li>
-                    <li class="option" data-value="Estrogonofe de grão de bico" data-price="12.90">Estrogonofe de grão de bico R$12,90</li>
-                    <li class="option" data-value="Queijo" data-price="12.90">Queijo R$12,90</li>
+                <li class="option" data-value="Cachorro-quente" data-price="10.90">Cachorro-quente R$10,90</li>
+                <li class="option" data-value="Carne de penela" data-price="18.90">Carne de panela R$18,90</li>
+                <li class="option" data-value="Estrogonofe de carne" data-price="23.90">Estrogonofe de carne R$23,90</li>
+                <li class="option" data-value="Lombo desfiado com BBQ" data-price="17.90">Lombo desfiado com BBQ R$17,90</li>
+                <li class="option" data-value="Estrogonofe de frango" data-price="17.90">Estrogonofe de frango R$17,90</li>
+                <li class="option" data-value="Bacon" data-price="15.90">Bacon R$15,90</li>
+                <li class="option" data-value="Calabresa" data-price="16.90">Calabresa R$16,90</li>
+                <li class="option" data-value="Frango desfiado" data-price="15.90">Frango desfiado R$15,90</li>
+                <li class="option" data-value="Bolonhesa" data-price="15.90">Bolonhesa R$15,90</li>
+                <li class="option" data-value="Alho-poró com gorgonzola" data-price="17.90">Alho-poró com gorgonzola R$17,90</li>
+                <li class="option" data-value="Pizza" data-price="12.90">Pizza R$12,90</li>
+                <li class="option" data-value="Estrogonofe de grão de bico" data-price="12.90">Estrogonofe de grão de bico R$12,90</li>
+                <li class="option" data-value="Queijo" data-price="12.90">Queijo R$12,90</li>
                 </ul>
             </fieldset>
             <fieldset>
@@ -272,7 +274,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <ul class="option-list">
                     <li><input type="checkbox" id="cebola-crispy" value="Cebola Crispy" data-price="4.00"><label for="cebola-crispy">Cebola Crispy (R$4,00)</label></li>
                     <li><input type="checkbox" id="alho-crocante" value="Alho crocante" data-price="3.00"><label for="alho-crocante">Alho crocante (R$3,00)</label></li>
-                    <li><input type="checkbox" id="azeitona" value="Azeitona" data-price="3.00"><label for="azeitona">Azeitona (R$3,00)</label></li>
+                    <li><input type="checkbox" id="azeitona" value="Azeitona" data-price="4.00"><label for="azeitona">Azeitona (R$4,00)</label></li>
                     <li><input type="checkbox" id="bacon" value="Bacon" data-price="7.00"><label for="bacon">Bacon (R$7,00)</label></li>
                     <li><input type="checkbox" id="cheddar" value="Cheddar" data-price="6.00"><label for="cheddar">Cheddar (R$6,00)</label></li>
                     <li><input type="checkbox" id="ervilha" value="Ervilha" data-price="2.50"><label for="ervilha">Ervilha (R$2,50)</label></li>
@@ -385,47 +387,47 @@ function atualizarResumoPedido() {
 
         const isRosti = pedido.name.includes('Rosti');
 
-       if (pedido.name === 'Monta sua Batata Intergaláctica') {
-    item.innerHTML = `<h4>${pedido.name} - R$${pedido.totalPrice}</h4>`;
-    item.innerHTML += `<p>Batata: ${pedido.batata || 'Não especificado'}</p>`;
-    item.innerHTML += `<p>Base: ${pedido.base || 'Não especificado'}</p>`;
-    item.innerHTML += `<p>Borda: ${pedido.borda || 'Não especificado'}</p>`;
-    item.innerHTML += `<p>Sabor: ${pedido.sabor || 'Não especificado'}</p>`;
-    if (pedido.adicionais) {
-        item.innerHTML += `<p>Adicionais: ${pedido.adicionais}</p>`;
-    }
-} else {
-    item.innerHTML = `<h4>${pedido.name} - R$${pedido.totalPrice}</h4>`;
-    if (pedido.sabor) {
-        item.innerHTML += `<p>Sabor: ${pedido.sabor}</p>`;
-    }
-    if (pedido.queijo) {
-        const label = isRosti ? 'Queijo' : 'Borda';
-        item.innerHTML += `<p>${label}: ${pedido.queijo}</p>`;
-    }
-    if (pedido.adicionais) {
-        item.innerHTML += `<p>Adicionais: ${pedido.adicionais}</p>`;
-    }
+        if (pedido.name === 'Monta sua Batata Intergaláctica') {
+            item.innerHTML = `<h4>${pedido.name} - R$${pedido.totalPrice}</h4>`;
+            item.innerHTML += `<p>Batata: ${pedido.batata || 'Não especificado'}</p>`;
+            item.innerHTML += `<p>Base: ${pedido.base || 'Não especificado'}</p>`;
+            item.innerHTML += `<p>Borda: ${pedido.borda || 'Não especificado'}</p>`;
+            item.innerHTML += `<p>Sabor: ${pedido.sabor || 'Não especificado'}</p>`;
+            if (pedido.adicionais) {
+                item.innerHTML += `<p>Adicionais: ${pedido.adicionais}</p>`;
+            }
+        } else {
+            item.innerHTML = `<h4>${pedido.name} - R$${pedido.totalPrice}</h4>`;
+            if (pedido.sabor) {
+                item.innerHTML += `<p>Sabor: ${pedido.sabor}</p>`;
+            }
+            if (pedido.queijo) {
+                const label = isRosti ? 'Queijo' : 'Borda';
+                item.innerHTML += `<p>${label}: ${pedido.queijo}</p>`;
+            }
+            if (pedido.adicionais) {
+                item.innerHTML += `<p>Adicionais: ${pedido.adicionais}</p>`;
+            }
 
-    // Só mostra bebida se não for pudim nem mousse
-    if (
-        !pedido.name.toLowerCase().includes('pudim') &&
-        !pedido.name.toLowerCase().includes('Mousse') &&
-        pedido.bebida
-    ) {
-        item.innerHTML += `<p>Bebida: ${pedido.bebida}</p>`;
-    }
+            // Só mostra bebida se não for pudim nem mousse
+            if (
+                !pedido.name.toLowerCase().includes('pudim') &&
+                !pedido.name.toLowerCase().includes('Mousse') &&
+                pedido.bebida
+            ) {
+                item.innerHTML += `<p>Bebida: ${pedido.bebida}</p>`;
+            }
 
-    // Sobremesa: pudim
-    if (pedido.name.toLowerCase().includes('pudim')) {
-        item.innerHTML += `<p>Sobremesa: pudim</p>`;
-    }
+            // Sobremesa: pudim
+            if (pedido.name.toLowerCase().includes('pudim')) {
+                item.innerHTML += `<p>Sobremesa: pudim</p>`;
+            }
 
-    // Sobremesa: mousse
-    if (pedido.name.toLowerCase().includes('mousse')) {
-        item.innerHTML += `<p>Sobremesa: mousse</p>`;
-    }
-}
+            // Sobremesa: mousse
+            if (pedido.name.toLowerCase().includes('mousse')) {
+                item.innerHTML += `<p>Sobremesa: mousse</p>`;
+            }
+        }
 
         item.innerHTML += `<button class="excluir-item" data-index="${index}">Excluir</button>`;
         pedidoItens.appendChild(item);
